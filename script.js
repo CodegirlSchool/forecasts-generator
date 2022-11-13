0a3838b7810abbc434d7793af99a690544c845f6
Alina-Murashko <116423988+Alina-Murashko@users.noreply.github.com>
2022-11-14 01:38:31 +0300

    Не добавляется в template

--- script.js
+++ script.js
@@ -13,3 +13,68 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const currentForecast = document.querySelector('.current-forecast');
+const currentForecastText = currentForecast.querySelector('h1');
+const currentForecastPercent = currentForecast.querySelector('p');
+
+const percent =  document.createElement('div');
+percent.classList.add('current-forecast');
+percent.textContent = 'Вероятность';
+
+const predictionLove = document.createElement('div');
+predictionLove.classList.add('current-forecast');
+predictionLove.textContent = 'В этом году любовь приобретет новый смысл!';
+
+const predictionJob = document.createElement('div');
+predictionJob.classList.add('current-forecast');
+predictionJob.textContent = 'В этом году ты найдешь работу мечты!';
+
+const predictionJS = document.createElement('div');
+predictionJS.classList.add('current-forecast');
+predictionJS.textContent = 'В этом году ты освоишь JS!';
+
+const getPredictions = document.querySelector(".forecast-btn");
+
+getPredictions.addEventListener('click', () => {
+    let predictionRandom = randomGeneration(1, 3);
+
+    if (predictionRandom === 1) {
+        currentForecastText.append(predictionLove);
+        currentForecastPercent.append(percent + randomGeneration(0, 100)/* Добавить сюда проценты при помощи str.endsWith(substr)*,
+        показыват почему то обьект вместо percent-вероятность*/ );
+
+    } else if (predictionRandom === 2) {
+        currentForecastText.append(predictionJob);
+        currentForecastPercent.append(percent + randomGeneration(0, 100));
+
+    } else if (predictionRandom === 3) {
+        currentForecastText.append(predictionJS);
+        currentForecastPercent.append(percent + randomGeneration(0, 100));
+    }
+
+    const prediction = currentForecastText;
+    const predictionPercentage = currentForecastPercent;
+    makeCardTemplate(prediction, predictionPercentage);
+
+})
+
+function randomGeneration(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const cardTemplate = document.querySelector("#forecast-item");
+
+function makeCardTemplate(prediction, predictionPercentage) {
+    const myCard = cardTemplate.content.cloneNode(true);
+    myCard.querySelector(h1).textContent = prediction;
+    myCard.querySelector(p).textContent = predictionPercentage;
+    return myCard;
+}
+
+
+
+
+
+
+
